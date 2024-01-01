d44b0cbf53adfdbda777cecee51e498fe3d7367a
user1 <yongjuni30@gmail.com>
2024-01-01 16:12:39 +0900

    auth.js_skeleton

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,22 @@
+const Users = require('../models/user');
 exports.postLogin = (req, res, next) => {  // TODO
-    const id = req.body.id;
-    const password = req.body.password;
-    console.log(id, password);
+    const {id,password} = req.body;
+    //db에 id이미 존재시 실패
+    const user = Users.GetUser(id);  //model의 getuser함수로 id에 대응하는 유저 반환
+    if(user) {
+        if (user.pasword===password)
+        {
+            //로그인 성공
+        }
+        else
+            res.status(400).send('wrong password');
+        
+    }
+    const newUser = {
+        id,
+        password,
+    }
+    //db에 저장
+    res.cookie();
+    res.redirect('/');
 };
