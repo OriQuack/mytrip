d29fc9a750c8a6b097cd5a322b09ae1ed5aa6531
user1 <yongjuni30@gmail.com>
2024-01-01 16:18:06 +0900

    user.js_class and finduser function

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
+
+
+
+class User {
+    constructor(id,password){
+        this.id=id;
+        this.password=password;
+    }
+
+    GetUser(userId) {
+        const db= getDb();
+        return db.collection('???')
+        .find({_id: new mongodb.ObjectId(userId)})
+        .next()
+        .then(user=> {
+            console.log(user);
+            return user;
+        })
+        .catch(err=> {
+            console.log(err);
+        });
+    };
+}
+
