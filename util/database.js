84754a0194595a5171fe0737809879388addcd10
Dohyun <kimdoh0423@naver.com>
2024-01-01 18:20:20 +0900

    Added database URI

--- util/database.js
+++ util/database.js
@@ -3,25 +3,26 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let db;
+const MONGODB_URI =
+    'mongodb+srv://admin:XjeD9tG6brrwden3@mytrip.e0j3pi1.mongodb.net/?retryWrites=true&w=majority';
 
-const mongoConnect = (callback)=> {
-    MongoClient.connect('계정')
-    .then(client => {
-        console.log('connected!');
-        db=client.db();
-        callback();
-    })
-    .catch(err=> {
-        console.log(err);
-        throw err;
-    });
-}
+const mongoConnect = (callback) => {
+    MongoClient.connect(MONGODB_URI)
+        .then((client) => {
+            console.log('connected!');
+            db = client.db();
+            callback();
+        })
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
+};
 
-const getDb = ()=> {
-    if(db)
-        return db;
-    throw "No database found!";
-}   
+const getDb = () => {
+    if (db) return db;
+    throw 'No database found!';
+};
 
-exports.mongoConnect = mongoConnect;//데이터베이스에 연결하고 그 연결을 저장
-exports.getDb = getDb;//연결된 데이터베이스를 리턴
+exports.mongoConnect = mongoConnect; //데이터베이스에 연결하고 그 연결을 저장
+exports.getDb = getDb; //연결된 데이터베이스를 리턴
