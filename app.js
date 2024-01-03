f3a434153708d695d8f76ba756274ab77a5ed415
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2024-01-03 20:16:00 +0900

    install session middleware

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const session =  require('express-session');
 const app = express();
 
 const authRoutes = require('./routes/auth');
 const planRoutes = require('./routes/plan');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(authRoutes);
 app.use(planRoutes);
 
+app.use(session({
+    secret: '***REMOVED***',
+    resave: false,
+    saveUninitialized: false,
+}))
+
 app.listen(3000);
+
