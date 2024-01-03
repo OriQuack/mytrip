a5aada46d7c2cc1c9ea915a1db42319a5e844117
Dohyun <kimdoh0423@naver.com>
2024-01-04 02:10:49 +0900

    added https sever

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const path = require('path');
 
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
-const session =  require('express-session');
+const session = require('express-session');
+
 const app = express();
 
+const options = require('./config/key_config').options;
 const authRoutes = require('./routes/auth');
 const planRoutes = require('./routes/plan');
 
@@ -12,11 +16,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(authRoutes);
 app.use(planRoutes);
 
-app.use(session({
-    secret: 'key key key',
-    resave: false,
-    saveUninitialized: false,
-}))
+app.use(
+    session({
+        secret: 'key key key',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
-app.listen(3000);
+const httpServer = http.createServer(app); // http 서버
+const httpsServer = https.createServer(options, app); // https 서버
 
+httpServer.listen(3000);
+httpsServer.listen(3001);
