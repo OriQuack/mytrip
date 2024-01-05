85d02a6563009546e7efe2aeacc7dd3efb8ac32b
Dohyun <kimdoh0423@naver.com>
2024-01-05 19:49:39 +0900

    changed getUser method

--- models/user.js
+++ models/user.js
@@ -2,26 +2,43 @@ const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb');
 
 class User {
-    constructor(id, password) {
-        this.id = id;
+    constructor(username, email, password) {
+        this.name = username;
+        this.email = email;
         this.password = password;
+    }
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
     }
     createUser() {
         const db = getDb();
         if (!this.getUser(this.id)) {
             console.log('id already exists!');
         } else {
-            db.collection('???').insertOne(this);
+            db.collection('users').insertOne(this);
         }
     }
-    getUser(userId) {
+    static getUserById(userId) {
         const db = getDb();
         return db
-            .collection('???')
-            .find({ _id: new mongodb.ObjectId(userId) })
+            .collection('users')
+            .findOne({ _id: new mongodb.ObjectId(userId) })
             .next()
             .then((user) => {
                 console.log(user);
+                return user;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    static getUserByEmail(userEmail) {
+        const db = getDb();
+        return db
+            .collection('users')
+            .findOne({ email: userEmail })
+            .then((user) => {
                 return user;
             })
             .catch((err) => {
