5b33d83d36daaad778bfb1e88f33658f984fb7a9
Dohyun <kimdoh0423@naver.com>
2024-01-05 20:45:52 +0900

    added comments

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,14 +37,14 @@ exports.postSignup = (req, res, next) => {
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
     if (password !== confirmPassword) {
-        // TODO: Send some error
+        // TODO: Send "password does not match" error
         return res.redirect('/signup');
     }
     Users.getUserByEmail(email)
         .then((userDoc) => {
             if (userDoc) {
                 // 해당 email을 가진 유저가 이미 존재
-                // TODO: Send some error
+                // TODO: Send "user already exists" error
                 return res.redirect('/signup');
             }
             return bcrypt
