b11ca30d5d0774cf3c018a79ba8aa78afacfbd97
Dohyun <kimdoh0423@naver.com>
2024-01-06 18:07:44 +0900

    added csrf module

--- app.js
+++ app.js
@@ -8,17 +8,19 @@ const bodyParser = require('body-parser');
 const mongoConnect = require('./util/database').mongoConnect;
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const app = express();
 const store = new MongoDBStore({
     uri: process.env.DB_URI,
     collection: 'sessions',
 });
+const csrfProtection = csrf();
 
 const options = require('./config/key_config').options;
-
 const authRoutes = require('./routes/auth');
 const planRoutes = require('./routes/plan');
+
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
@@ -27,12 +29,12 @@ app.use(
         store: store,
     })
 );
-
+app.use(csrfProtection);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(authRoutes);
 app.use(planRoutes);
-
 
 mongoConnect(() => {
     http.createServer(app).listen(process.env.HTTP_PORT); // http 서버
