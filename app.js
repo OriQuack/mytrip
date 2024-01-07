e4dae6848a7db136ac79ffc70bf634a732331e70
Dohyun <kimdoh0423@naver.com>
2024-01-07 15:01:05 +0900

    fixed minor error

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 
+const User = require('./models/user');
+
 const app = express();
 const store = new MongoDBStore({
     uri: process.env.DB_URI,
@@ -45,12 +47,6 @@ app.use((req, res, next) => {
         })
         .catch((err) => console.log(err));
 });
-
-// app.use((req, res, next) => {
-//     res.locals.isAuthenticated = req.session.isLoggedIn;
-//     res.locals.csrfToken = req.csrfToken();
-//     next();
-// });
 
 app.use(authRoutes);
 app.use(planRoutes);
