2b19d74ed81130f5544f40796a11598bd127c08d
Dohyun <kimdoh0423@naver.com>
2024-01-12 01:01:04 +0900

    changed cors to 5173 port

--- config/cors_config.js
+++ config/cors_config.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 // 특정 주소 접근 허용
 exports.options = cors({
     origin: [
-        'https://mytripping.vercel.app/', //
-        'http://localhost:5000',
+        'http://localhost:5173',
     ],
 });
