96af5860bf2fbdce414479090d49572a82c9ac7a
Dohyun <kimdoh0423@naver.com>
2024-01-12 17:17:04 +0900

    added jwt token and authenticate middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const Users = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { validationResult } = require('express-validator');
 
@@ -7,52 +8,42 @@ exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        console.log("Invalid Email.");
-        return res.redirect('/login');
-        // return res.status(422).render('auth/login', {
-        //     path: '/login',
-        //     pageTitle: 'login',
-        //     errorMessage: errors.array()[0].msg
-        //     oldInput: {email: email, password: password } //Keep user data
-        // });
-    }
-
     Users.getUserByEmail(email).then((user) => {
         if (!user) {
-            // TODO: send "Invalid email or password" error
-            console.log("Email doesn't match with any users");
-            return res.redirect('/login');
+            return res.status(404).json({ message: 'User not found' });
         }
-        // email에 대응하는 유저 존재
         bcrypt
             .compare(password, user.password)
             .then((doMatch) => {
                 if (doMatch) {
-                    req.session.isLoggedIn = true;
-                    req.session.user = user;
-                    console.log('session init complete');
-                    return req.session.save((err) => {
-                        console.log(err);
-                        res.redirect('/');
-                    });
+                    const accessToken = jwt.sign(
+                        { userEmail: email },
+                        process.env.TOEKN_SECRET,
+                        {
+                            expiresIn: '15m',
+                        }
+                    );
+                    const refreshToken = jwt.sign(
+                        { userEmail: email },
+                        process.env.TOEKN_SECRET,
+                        {
+                            expiresIn: '1d',
+                        }
+                    );
+                    return res
+                        .status(200)
+                        .cookie('refreshToken', refreshToken, {
+                            httpOnly: true,
+                        })
+                        .header('Authorization', accessToken)
+                        .json({ username: user.username });
                 }
-                // TODO: send "Invalid email or password" error
-                console.log('Invalid email or password');
-                res.redirect('/login');
+                return res.status(401).json({ message: 'Incorrect password' });
             })
             .catch((err) => {
                 console.log(err);
-                return res.redirect('/login');
+                return res.status(400).json({ message: 'Bad request' });
             });
-    });
-};
-
-exports.postLogout = (req, res, next) => {
-    req.session.destroy((err) => {
-        console.log(err);
-        res.redirect('/');
     });
 };
 
@@ -60,21 +51,6 @@ exports.postSignup = (req, res, next) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
-    const errors = validationResult(req);
-
-    //Check the validation result
-    if (!errors.isEmpty()) {//Invalid email
-        console.log("Invalid signup information");
-        console.log(errors.array());
-        return res.redirect('/signup');
-        // return res.status(422).render('auth/signup', {
-        //     path: '/signup',
-        //     pageTitle: 'Signup',
-        //     errorMessage: errors.array()[0].msg
-        //     oldInput: {username: username, email: email, password: password, confirmPassword: confirmPassword}
-        // });
-    }
 
     bcrypt
         .hash(password, 12)
@@ -83,11 +59,16 @@ exports.postSignup = (req, res, next) => {
             return user.save();
         })
         .then((result) => {
-            console.log('signup complete!');
-            res.redirect('/login');
+            return res
+                .status(200)
+                .cookie('refreshToken', refreshToken, {
+                    httpOnly: true,
+                })
+                .header('Authorization', accessToken)
+                .json({ username: username });
         })
         .catch((err) => {
             console.log(err);
-
-        })
+            return res.status(500);
+        });
 };
