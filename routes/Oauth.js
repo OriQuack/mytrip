d10b496189a277a35ce577486cd6fce2871982f1
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2024-01-15 20:17:21 +0900

    Create Oauth.js

---
+++ routes/Oauth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+
+const {OauthController} = require('../controllers/Oauth');
+const router = express.Router();
+
+//router.get('/naver.callback',OauthController.NaverLogin);
+
+module.exports = router;
