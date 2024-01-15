ba650b389ee0b509b818285c48ce1babbceaabed
Dohyun <kimdoh0423@naver.com>
2024-01-15 11:34:51 +0900

    changed cors options

--- config/cors_config.js
+++ config/cors_config.js
@@ -4,4 +4,5 @@ const cors = require('cors');
 exports.options = cors({
     origin: ['http://localhost:5173', 'http://localhost:5174', 'https://mytripping.vercel.app'],
     exposedHeaders: ['Authorization'],
+    credentials: true,
 });
