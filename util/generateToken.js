be6a5a5e84e3838d39a2ff451365ad82175b5f4d
Dohyun <kimdoh0423@naver.com>
2024-01-16 00:41:03 +0900

    changed algorithm for security

--- util/generateToken.js
+++ util/generateToken.js
@@ -4,12 +4,20 @@ const genAccessToken = (email) => {
     const payload = { userEmail: email };
     return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '30m',
+        notBefore: '0',
+        algorithm: 'HS256',
+        // issuer
+        // audience
     });
 };
 
 const genRefreshToken = () => {
     return jwt.sign({}, process.env.REFRESH_TOKEN_SECRET, {
         expiresIn: '3d',
+        notBefore: '0',
+        algorithm: 'HS256',
+        // issuer
+        // audience
     });
 };
 
