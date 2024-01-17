3922227886af8a91c000bb88dd4765673dec6058
user1 <yongjuni30@gmail.com>
2024-01-17 22:37:33 +0900

    get->post

--- routes/Oauth.js
+++ routes/Oauth.js
@@ -4,6 +4,6 @@ const express = require('express');
 const OauthController = require('../controllers/Oauth');
 const router = express.Router();
 
-router.get('/naver',OauthController.NaverLogin);
+router.post('/naver',OauthController.NaverLogin);
 
 module.exports = router;
