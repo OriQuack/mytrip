7e84c8aea2ae9cffa86dedc21777ca47a5609d02
Dohyun <kimdoh0423@naver.com>
2024-01-21 13:15:45 +0900

    erased dummy route

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,9 +1,7 @@
+const Plan = require('../models/plan');
+
 exports.getIndex = (req, res, next) => {
     // TODO
     res.send('<h1>Main</h1>'); // Connect to frontend
 };
 
-exports.getProtectedExample = (req, res, next) => {
-    // DUMMY
-    res.send(`Protected route accessed by ${req.user.username}`);
-};
