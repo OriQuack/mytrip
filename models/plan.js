0d28744c0ba4ba1796d3b9a02c17f80fb602194c
Dohyun <kimdoh0423@naver.com>
2024-01-22 22:28:59 +0900

    changed plan model

--- models/plan.js
+++ models/plan.js
@@ -3,16 +3,20 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Plan {
-    constructor({ name, _id, ownerId, city, date, likes, scraps, totalCost, isPublic, schedule }) {
+    constructor({ name, _id, ownerId, city, time: date, likes, totalCost, isPublic, schedule }) {
         this.name = name;
         this._id = _id ? _id : null;
         this.ownerId = ownerId;
         this.city = city;
-        this.date = date;
+        this.date = date; // 날짜
+        this.period = period; // 며칠동안
+        // this.season = -> 날짜에 따라 계산 TODO
         this.likes = likes ? likes : 0;
+        this.views = views ? views : 0;
         this.scraps = scraps ? scraps : 0;
         this.totalCost = totalCost;
         this.isPublic = isPublic;
+        this.isComplete = isComplete;
         this.schedule = schedule;
     }
 
