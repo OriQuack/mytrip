46eda34c55b5f981887687b98fffa8e367ed362c
Dohyun <kimdoh0423@naver.com>
2024-01-27 16:55:00 +0900

    modified user model

--- models/user.js
+++ models/user.js
@@ -32,14 +32,7 @@ class User {
     save() {
         const db = getDb();
         if (this._id) {
-            const existingUser = db.collection('users').findOne({ email: this.email });
-            console.log(existingUser);
-            if (existingUser) {
-                console.log('user exist');
-                return res.status(300);
-            }
             // User exists -> update user
-            console.log('user exists');
             return db.collection('users').updateOne({ _id: this._id }, { $set: this });
         }
         return db.collection('users').insertOne(this);
