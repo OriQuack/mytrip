8b77c6c2bcb28e6c0c8362a46ed8b8b804ceda4b
yoonjiii <yoonjian17@gmail.com>
2024-01-29 21:34:41 +0900

    fix minor

--- controllers/community.js
+++ controllers/community.js
@@ -6,26 +6,26 @@ const City = require('../models/city');
 exports.getAllPosts = (req, res, next) => { //기본적으로는 최신순
     Plan.getAllSortedByDate()
         .then(posts => {
-            res.status(200).json({
+            return res.status(200).json({
                 posts: posts
             });
         })
         .catch(err => {
             console.error(err);
-            res.status(500).json({ message: 'Internal server error' });
+            return res.status(500).json({ message: 'Internal server error' });
         });
 };
 
 exports.getAllPostsByLikes = (req, res, next) => { //좋아요순
     Plan.getAllSortedByLikes()
         .then(posts => {
-            res.status(200).json({
+            return res.status(200).json({
                 posts: posts
             });
         })
         .catch(err => {
             console.error(err);
-            res.status(500).json({ message: 'Internal server error' });
+            return res.status(500).json({ message: 'Internal server error' });
         });
 };
 
@@ -33,12 +33,12 @@ exports.getPostById = (req, res, next) => {
     const postId = req.params.postId;
     Plan.getPlanById(postId)
         .then(post => {
-            res.status(200).json({
+            return res.status(200).json({
                 post: post
             });
         })
         .catch(err => {
             console.error(err);
-            res.status(500).json({ message: 'Internal server error' });
+            return res.status(500).json({ message: 'Internal server error' });
         });
 };
