b0973b04216078b627ba5aa336a03330458773fe
user1 <yongjuni30@gmail.com>
2024-01-29 20:48:52 +0900

    asd

--- routes/destination.js
+++ routes/destination.js
@@ -4,8 +4,8 @@ const destController = require('../controllers/destination');
 
 const router = express.Router();
 
-router.get('/planning/data/:region', destController.getDestByCity); //도시 검색시 추천여행지
+router.get('/data/:region', destController.getDestByCity); //도시 검색시 추천여행지
 
-router.get('/planning/data/:destination', destController.getDestination); //여행지 검색시 
+router.get('/data/:destination', destController.getDestination); //여행지 검색시 
 
 module.exports = router;
