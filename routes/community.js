018141c664bde98e37c74b20af945b954b4aa2ee
Dohyun <kimdoh0423@naver.com>
2024-01-30 14:53:07 +0900

    added checkLogin to community routes

--- routes/community.js
+++ routes/community.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const communityController = require('../controllers/community');
+const checkLogin = require('../middleware/checkLogin');
 const router = express.Router();
 
-router.get('/', communityController.getAllPosts);
-router.get('/likes', communityController.getAllPostsByLikes);
-router.get('/:postId', communityController.getPostById);
+router.get('/', checkLogin, communityController.getAllPosts);
+
+router.get('/likes', checkLogin, communityController.getAllPostsByLikes);
+
+router.get('/:postId', checkLogin, communityController.getPostById);
 
 module.exports = router;
