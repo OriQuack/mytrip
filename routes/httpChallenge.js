ac655d64c262ed09b83091125da96b3e5bee7d32
Dohyun <kimdoh0423@naver.com>
2024-02-07 17:28:15 +0900

    httpChallenge added

---
+++ routes/httpChallenge.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('node:js');
+
+const router = express.Router();
+
+router.get(
+    '/.well-known/acme-challenge/C-vZGv3LynJUWBXKND_xNO18r-BRnNa8Icd2U640eOw',
+    (req, res) => {
+        return res.send(
+            fs.readFileSync(
+                './.well-known/acme-challenge/C-vZGv3LynJUWBXKND_xNO18r-BRnNa8Icd2U640eOw'
+            )
+        );
+    }
+);
