37529774e8eaf0386e4f420558be81294d1cf7bc
Dohyun <kimdoh0423@naver.com>
2024-02-07 18:48:56 +0900

    added https

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
-// const httpsOptions = require('./config/key_config').options;
+const httpsOptions = require('./config/key_config').options;
 const corsOptions = require('./config/cors_config').options;
 const authRoutes = require('./routes/auth');
 const planRoutes = require('./routes/plan');
@@ -36,5 +36,5 @@ app.use(httpChallenge);
 
 mongoConnect(() => {
     http.createServer(app).listen(process.env.HTTP_PORT); // http 서버
-    // https.createServer(httpsOptions, app).listen(process.env.HTTPS_PORT); // https 서버
+    https.createServer(httpsOptions, app).listen(process.env.HTTPS_PORT); // https 서버
 });
--- config/key_config.js
+++ config/key_config.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-// 현재 스크립트의 디렉토리를 기반으로한 상대 경로
-// const certPath = path.join(__dirname, 'config', 'secure', 'rootca', 'rootca.crt');
-// const keyPath = path.join(__dirname,'config','secure','rootca','rootca.key');
 module.exports.options = {
     // https 암호화 키 (dummy 키)
-    key: fs.readFileSync('./config/secure/rootca.key'),
-    cert: fs.readFileSync('./config/secure/rootca.crt'),
+    key: fs.readFileSync('/etc/letsencrypt/live/commitmytripplanner.shop/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/commitmytripplanner.shop/fullchain.pem'),
 };
